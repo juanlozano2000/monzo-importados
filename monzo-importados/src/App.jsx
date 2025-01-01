34bfdec4b112ef4960b5378ebbfe1d4ae217e29e
juanlozano2000 <juaniloza83@gmail.com>
2025-01-01 18:46:22 -0300

    implementoFooter

--- monzo-importados/src/App.jsx
+++ monzo-importados/src/App.jsx
@@ -1,16 +1,24 @@
 //App.jsx
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header.jsx'
 import Navbar from './components/Navbar.jsx'
-import Products from './components/Products.jsx'
+import Footer from './components/Footer.jsx'
+import Products from './pages/Products.jsx'
 import './App.css'
 
 function App() {
 
   return (
     <>
-    <Header />
-    <Navbar />
-    <Products />
+    <BrowserRouter>
+      <Header />
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Products />} />
+        <Route path="/products" element={<Products />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
     </>
   )
 }
--- monzo-importados/src/components/Navbar.jsx
+++ monzo-importados/src/components/Navbar.jsx
@@ -1,4 +1,3 @@
-//Navbar.jsx
 import React from 'react';
 import '../assets/Navbar.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -7,22 +6,22 @@ import { faSprayCanSparkles, faVial, faPlus, faMobileAlt } from '@fortawesome/fr
 const Navbar = () => {
     // Icon data array
     const icons = [
-        { icon: faSprayCanSparkles, label: 'Perfumes', color: 'text-blue-500' },
-        { icon: faVial, label: 'Perfumeros', color: 'text-blue-500' },
-        { icon: faMobileAlt, label: 'Tecnología', color: 'text-blue-500' }, // Corrected icon name
-        { icon: faPlus, label: 'Otros', color: 'text-blue-500' }
+        { href: '#Perfumes', icon: faSprayCanSparkles, label: 'Perfumes', color: 'text-blue-500' },
+        { href: '#Perfumeros', icon: faVial, label: 'Perfumeros', color: 'text-blue-500' },
+        { href: '#Tecnologia', icon: faMobileAlt, label: 'Tecnología', color: 'text-blue-500' },
+        { href: '#Otros', icon: faPlus, label: 'Todo', color: 'text-blue-500' }
     ];
 
     return (
         <nav className="bg-white overflow-x-auto py-3 flex justify-center items-center">
             <div className="flex justify-center items-center space-x-2 md:space-x-4">
                 {icons.map((item, index) => (
-                    <div key={index} className="text-center flex flex-col items-center w-20 md:w-24">
+                    <a href={item.href} key={index} className="text-center flex flex-col items-center w-20 md:w-24">
                         <div className="w-8 h-8 md:w-10 md:h-10 lg:w-12 lg:h-12 rounded-full bg-gray-200 flex items-center justify-center">
                             <FontAwesomeIcon icon={item.icon} className={item.color} />
                         </div>
                         <p className="text-xs md:text-sm mt-1">{item.label}</p>
-                    </div>
+                    </a>
                 ))}
             </div>
         </nav>
