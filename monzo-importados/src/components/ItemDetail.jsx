4c1c77c46c5fe47ff53e475c98f955808cfa6fe4
juanlozano2000 <juaniloza83@gmail.com>
2025-01-08 06:49:19 -0300

    funcionalidad sin stock en detail

--- monzo-importados/src/components/ItemDetail.jsx
+++ monzo-importados/src/components/ItemDetail.jsx
@@ -41,7 +41,14 @@ const ItemDetail = () => {
     }
 
     return (
-        <div className="max-w-4xl mx-auto p-4 bg-white shadow-md rounded-md mt-3 md:mt-10 mb-10">
+        <div className="max-w-4xl mx-auto p-4 bg-white shadow-md rounded-md mt-3 md:mt-10 mb-10 relative">
+            {/* Indicador Sin Stock */}
+            {!item.status_active && (
+                <div className="absolute top-4 left-4 bg-red-500 text-white text-sm px-3 py-1 rounded-md z-10">
+                    Sin stock
+                </div>
+            )}
+
             <section className="py-8 bg-white md:py-16 dark:bg-gray-900 antialiased">
                 <div className="max-w-screen-xl px-4 mx-auto 2xl:px-0">
                     <div className="lg:grid lg:grid-cols-2 lg:gap-8 xl:gap-16">
@@ -95,10 +102,15 @@ const ItemDetail = () => {
                                 {/* Botón de carrito */}
                                 <button
                                     onClick={() => addToCart(item)}
-                                    className="mt-4 sm:mt-0 sm:ml-4 bg-blue-500 hover:bg-blue-700 text-white font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 w-full sm:w-auto flex items-center justify-center"
+                                    disabled={!item.status_active}
+                                    className={`mt-4 sm:mt-0 sm:ml-4 font-medium rounded-lg text-sm px-5 py-2.5 flex items-center justify-center w-full sm:w-auto ${
+                                        item.status_active
+                                            ? 'bg-blue-500 hover:bg-blue-700 text-white'
+                                            : 'bg-gray-400 text-gray-300 cursor-not-allowed'
+                                    }`}
                                 >
                                     <FontAwesomeIcon icon={faShoppingCart} className="mr-2" size="lg" />
-                                    Añadir al carrito
+                                    {item.status_active ? 'Añadir al carrito' : 'No disponible'}
                                 </button>
                             </div>
 
