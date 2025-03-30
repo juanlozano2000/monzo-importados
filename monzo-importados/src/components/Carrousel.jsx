c29ca1941ce5c8cbcef5edd4b6a312763227c030
juanlozano2000 <juaniloza83@gmail.com>
2025-03-30 17:03:52 -0300

    carrousel Yara

--- monzo-importados/src/components/Carrousel.jsx
+++ monzo-importados/src/components/Carrousel.jsx
@@ -8,9 +8,11 @@ const Carrousel = () => {
     const [activeIndex, setActiveIndex] = useState(0);
     const images = [
         "/imgs/carrousel_1.png",
-        "/imgs/carrouselDecants.png"
+        "/imgs/carrouselYara.png",
+        "/imgs/carrouselDecants.png",
+        "/imgs/carrouselCompresor.png"
     ];
-    const delay = 4000;  // Delay in milliseconds between slides
+    const delay = 5500;  // Delay in milliseconds between slides
 
     const nextSlide = () => {
         setActiveIndex((current) => (current + 1) % images.length);  // Move to the next slide
@@ -45,22 +47,22 @@ const Carrousel = () => {
                     <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 3" data-carousel-slide-to="2"></button>
                 </div>
                 
-                <button type="button" onClick={prevSlide} className="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
+                {/* <button type="button" onClick={prevSlide} className="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
                     <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
                         <svg className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" fill="none" viewBox="0 0 6 10">
                             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 1 1 5l4 4"/>
                         </svg>
                         <span className="sr-only">Previous</span>
                     </span>
-                </button>
-                <button type="button" onClick={nextSlide} className="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
+                </button> */}
+                {/* <button type="button" onClick={nextSlide} className="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
                     <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
                         <svg className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" fill="none" viewBox="0 0 6 10">
                             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 9 4-4-4-4"/>
                         </svg>
                         <span className="sr-only">Next</span>
                     </span>
-                </button>
+                </button> */}
             </div>
         </div>
     )
